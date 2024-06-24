79642250ee3ba6e6362830f9c53d61ce30421c4f
Silvathor <silvablocks@gmail.com>
2024-06-24 18:27:07 +0200

    connexion bdd test

--- database/connexion.php
+++ database/connexion.php
@@ -1,34 +1,26 @@
 <?php
 
-$DB_HOST = "db";
+// $DB_HOST = "host";
+// $DB_USERNAME = "user";
+// $DB_PASSWORD = "pass";
+// $DB_NAME = "EpargneControle";
+$DB_PORT = "3306";
+
+$DB_HOST = "host";
 $DB_USERNAME = "user";
-$DB_PASSWORD = "pass";
+$DB_PASSWORD = "password";
 $DB_NAME = "epargne-controle";
 
 global $db;
 
 // Use env variables to connect to the database
 try {
-    $cnx = "mysql:host={$DB_HOST};dbname={$DB_NAME}";
+    $cnx = 'mysql:host='.$DB_HOST.';port='.$DB_PORT.';dbname='.$DB_NAME.'';
     $options = array(
         PDO::MYSQL_ATTR_SSL_CA => $_SERVER['DOCUMENT_ROOT'] . "../../../../etc/ssl/certs/ca-certificates.crt", // TODO: Change this path
     );
-    $db = new PDO($cnx, $DB_USERNAME, $DB_PASSWORD, $options);
+    $db = new PDO($cnx, $DB_USERNAME, $DB_PASSWORD);
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
     exit();
 }
-
-// // Query to fetch list of tables
-// $query = "SHOW TABLES";
-// $stmt = $pdo->query($query);
-// $tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
-
-// if (!empty($tables)) {
-//     echo "Tables in the database:\n";
-//     foreach ($tables as $table) {
-//         echo "- $table\n";
-//     }
-// } else {
-//     echo "Successfully ran the query. No tables were found in the database.\n";
-// }
