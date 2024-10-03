9b4c8f6947efd92e3703d4dce5bcfd4709c47898
Silvathor <silvablocks@gmail.com>
2024-10-03 20:30:54 +0200

    Refactor transaction.php to improve order assignment logic and add comments
    Refactor generics.scss to remove unnecessary CSS rules and improve readability

--- controler/creating_elements/transaction.php
+++ controler/creating_elements/transaction.php
@@ -18,12 +18,17 @@ $order = null;
 if ($from->getType() == 0 && $to->getType() == 1) {
     $order = 0;
 }
-else if ($from->getType() == 1 && $to->getType() == 0) {
-    $order = 7;
-}
-else {
+if ($from->getType() == 1 && $to->getType() == 1) {
     $order = 6;
 }
+else if ($to->getType() == 0) {
+    $order = 7;
+}
+
+// 0 -> 1 : 0 Saving
+// 0 -> 0 : 7 Withdrawal
+// 1 -> 0 : 7 Withdrawal
+// 1 -> 1 : 6 Other
 
 Operation::createOperation($_GET['label'], $_GET['date'], -$_GET['amount'], $order, 0, $_GET['from']);
 Operation::createOperation($_GET['label'], $_GET['date'], $_GET['amount'], $order, 0, $_GET['to']);
